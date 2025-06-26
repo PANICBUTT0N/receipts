2b386c5b1acf3acbb5324d7185bf6b15be7f6795
PANICBUTT0N <82723611+PANICBUTT0N@users.noreply.github.com>
2025-06-26 17:19:20 -0400

    Create receipts grid and random receipt texture generator

--- server/static/scripts.js
+++ server/static/scripts.js
@@ -1,18 +1,26 @@
+function skinPattern(receiptBox) {
+	const xCoord = Math.floor(Math.random() * -1033);
+	const yCoord = Math.floor(Math.random() * -500);
+	receiptBox.style.backgroundPosition = `${xCoord}px ${yCoord}px`;
+}
+
 function getAllReceipts() {
 	fetch('http://localhost:5000/api/get_all_receipts')
 		.then(res => res.json())
 		.then(data => {
-			const tableBody = document.querySelector('#all-receipts tbody');
+			console.log(data);
+			const receiptsGrid = document.querySelector('.receipts-grid');
 
 			data.forEach(receipt => {
-				const row = document.createElement('tr');
-				let cells = [receipt.id, receipt.item, receipt.store, receipt.price, receipt.date];
-				cells.forEach(cellData => {
-					let td = document.createElement('td');
-					td.textContent = cellData;
-					row.appendChild(td);
-				});
-				tableBody.appendChild(row);
+				const anchor = document.createElement('a');
+				anchor.href = "https://youtube.com"
+				const receiptBox = document.createElement('div');
+				receiptBox.textContent = receipt.store
+				receiptBox.className = 'box'
+				skinPattern(receiptBox)
+				let cells = [receipt.id, receipt.date, receipt.items, receipt.total, receipt.store, receipt.address, receipt.phone, receipt.payment_method];
+				anchor.appendChild(receiptBox);
+				receiptsGrid.appendChild(anchor);
 			});
 		});
 }
