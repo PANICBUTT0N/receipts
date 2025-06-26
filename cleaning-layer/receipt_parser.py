05e1c1179dbb2d09a876a72d2ea761a15c5d8118
Jadon Xu <jadonjxu@gmail.com>
2025-06-25 21:46:56 -0400

    Add folder and files for cleaning layer

---
+++ cleaning-layer/receipt_parser.py
@@ -0,0 +1,38 @@
+import json
+from PIL import Image
+import receipt_parser_utils
+
+class ReceiptParser:
+    def __init__(self, ocr_json_path: str, image_path):
+        with open(ocr_json_path, "r") as f:
+            self.ocr_data = json.load(f)
+
+        # image width/height
+        with Image.open(image_path) as img:
+            self.width, self.height = img.size
+
+        # ocr data
+        self.raw_texts = self.ocr_data["rec_texts"]
+        self.confidences = self.ocr_data["rec_scores"]
+        self.bboxes = self.ocr_data["rec_polys"]
+
+        # combining data into list of dicts for each text box
+        self.entries = self._combine_data()
+
+    def _combine_data(self):
+        return [
+            {"text": t, "confidence": c, "bbox": b}
+            for t, c, b in zip(self.raw_texts, self.confidences, self.bboxes)
+        ]
+    
+    def extract_store_name(self):
+        pass
+
+    def __str__(self):
+        return str(self.entries[:5])
+    
+json_path = "ocr/ocr_output/receipt1/receipt1_res.json"
+img_path = "receipts/receipt1.png"
+receiptParser = ReceiptParser(json_path, img_path)
+
+print(receiptParser)
