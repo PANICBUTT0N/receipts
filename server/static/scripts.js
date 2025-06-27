80b531c987d6cab1aeffb6b39eef51c67802ef48
PANICBUTT0N <82723611+PANICBUTT0N@users.noreply.github.com>
2025-06-26 21:09:58 -0400

    Style receipt boxes on homepage, close #8

--- server/static/scripts.js
+++ server/static/scripts.js
@@ -13,10 +13,59 @@ function getAllReceipts() {
 
 			data.forEach(receipt => {
 				const anchor = document.createElement('a');
-				anchor.href = "https://youtube.com"
+				anchor.href = 'receipt/'+ receipt.id
 				const receiptBox = document.createElement('div');
-				receiptBox.textContent = receipt.store
 				receiptBox.className = 'box'
+
+				const storeName = document.createElement('p')
+				storeName.textContent = receipt.store.toUpperCase()
+				storeName.className = 'store-name'
+
+				const date = document.createElement('p')
+				date.textContent = receipt.date
+				date.className = 'date'
+
+				const address = document.createElement('p')
+				address.textContent = receipt.address
+				address.className = 'address'
+
+				const itemsContainer = document.createElement('div')
+
+				const itemsArray = receipt.items
+				itemsArray.forEach(item => {
+					const items = document.createElement('div')
+					items.className = 'items'
+					const itemName = document.createElement('p')
+					itemName.className = 'item-name'
+					const itemPrice = document.createElement('p')
+					itemPrice.className = 'item-price'
+					const itemQuantity = document.createElement('p')
+					itemQuantity.className = 'item-quantity'
+					itemName.textContent = item.name
+					itemPrice.textContent = '$' + item.price
+					itemQuantity.textContent = item.quantity
+
+					items.appendChild(itemName)
+					items.appendChild(itemQuantity)
+					items.appendChild(itemPrice)
+
+					itemsContainer.appendChild(items)
+				})
+
+
+				const total = document.createElement('p')
+				total.textContent = receipt.total
+				total.className = 'total'
+
+				receiptBox.appendChild(storeName)
+				receiptBox.appendChild(address)
+				receiptBox.appendChild(date)
+				receiptBox.appendChild(itemsContainer)
+				receiptBox.appendChild(total)
+
+				address.textContent = receipt.address
+				address.className = 'address'
+
 				skinPattern(receiptBox)
 				let cells = [receipt.id, receipt.date, receipt.items, receipt.total, receipt.store, receipt.address, receipt.phone, receipt.payment_method];
 				anchor.appendChild(receiptBox);
@@ -25,3 +74,4 @@ function getAllReceipts() {
 		});
 }
 document.addEventListener('DOMContentLoaded', getAllReceipts);
+
